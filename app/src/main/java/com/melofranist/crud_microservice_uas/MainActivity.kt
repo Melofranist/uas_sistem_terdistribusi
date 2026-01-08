4f68ed66e5276f6a31a1a4129fe719061c7f3dca
Melofranist <157810697+Melofranist@users.noreply.github.com>
2026-01-08 22:18:49 +0700

    Initial Commit

---
+++ app/src/main/java/com/melofranist/crud_microservice_uas/MainActivity.kt
@@ -0,0 +1,47 @@
+package com.melofranist.crud_microservice_uas
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.melofranist.crud_microservice_uas.ui.theme.Crud_microservice_uasTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            Crud_microservice_uasTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    Crud_microservice_uasTheme {
+        Greeting("Android")
+    }
+}
